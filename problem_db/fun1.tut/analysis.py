26db533fdb8c138c03bc0cddc306c6407ccbdb9c
sapi <sean.purdon@gmail.com>
2014-12-10 21:03:25 -0800

    Cleaned up analysis to use new print/input idea

--- problem_db/fun1.tut/analysis.py
+++ problem_db/fun1.tut/analysis.py
@@ -6,26 +6,14 @@ class CodeVisitor(ast.NodeVisitor):
         self.count = 1
         self.seen_inc = []
         self.seen_double = []
-        self.did_assign = False
-        self.in_assign = False
 
     def visit_Call(self, t):
-        if self.in_assign:
-            if t.func.id == 'double':
-                self.seen_double.append(self.count)
-                self.count += 1
-            elif t.func.id == 'increment':
-                self.seen_inc.append(self.count)
-                self.count += 1
-            for arg in t.args:
-                self.visit(arg)
-
-    def visit_Assign(self, t):
-        if t.targets[0].id == 'm':
-            self.in_assign = True
-            self.did_assign = True
-            self.visit(t.value)
-            self.in_assign = False
+        if t.func.id == 'double':
+            self.seen_double.append(self.count)
+            self.count += 1
+        elif t.func.id == 'increment':
+            self.seen_inc.append(self.count)
+            self.count += 1
 
 
 class Fun1Analyser(CodeAnalyser):
@@ -33,17 +21,18 @@ class Fun1Analyser(CodeAnalyser):
         astree = ast.parse(text)
         self.visitor.visit(astree)
 
-        if not self.visitor.did_assign:
-            self.add_error("You have not assigned to the variable m")
-        if len(self.visitor.seen_double) == 0:
+        if not self.visitor.seen_double:
             self.add_error("You need to use the double function in your single assignment statement")
-        if len(self.visitor.seen_inc) == 0:
+        if not self.visitor.seen_inc:
             self.add_error("You need to use the increment function in your single assignment statement")
+
         if len(self.visitor.seen_double) > 1:
             self.add_error("You only need to use double once")
         if len(self.visitor.seen_inc) > 1:
             self.add_error("You only need to use increment once")
-        if self.visitor.seen_double[0] > self.visitor.seen_inc[0]:
+
+        if self.visitor.seen_double and self.visitor.seen_inc \
+                and self.visitor.seen_double[0] > self.visitor.seen_inc[0]:
             self.add_error("You should be using increment inside the use of double")
 
 
