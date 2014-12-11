3ddcbea51531d8609eca7cda0579818195570ab5
sapi <sean.purdon@gmail.com>
2014-12-10 19:02:48 -0800

    Added newlines to expected results

--- problem_db/fun1.tut/tests.py
+++ problem_db/fun1.tut/tests.py
@@ -7,13 +7,13 @@ class Fun1Tests(StudentTestCase):
         _ = self.run_student_code(input_text='2')
 
         # check that they printed the output correctly
-        expected_output = '6'  # 2*(2 + 1)
+        expected_output = '6\n'  # 2*(2 + 1)
         self.assertEqual(self.standard_output, expected_output)
 
     def test_fun1_hidden(self):
         _ = self.run_student_code(input_text='0')
 
-        expected_output = '2'  # 2*(0 + 1)
+        expected_output = '2\n'  # 2*(0 + 1)
         self.assertEqual(self.standard_output, expected_output)
 
 
