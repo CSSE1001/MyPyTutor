5790633c0d470b1d60a61bfae3591b09bafb6f53
Sean Purdon <sean.purdon@gmail.com>
2014-12-11 22:44:30 +1000

    Fixed bug causing tutorials.name key to lose value
    It turns out that filter is a generator in Python 3.
    Oops...

--- code/tutorlib/configuration.py
+++ code/tutorlib/configuration.py
@@ -95,8 +95,7 @@ def unwrap_value(section, option, value):
 
     # TODO: I vaguely remember a bug using is on builtins, should check
     if special_type is list:
-        elems = value.split(',')
-        return filter(None, elems)
+        return [elem for elem in value.split(',') if elem]
     elif special_type is int:
         return int(value)
 
