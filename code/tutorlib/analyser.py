f3a007d18e2ee2395b3a430870e505e47711504f
Sean Purdon <sean.purdon@gmail.com>
2014-12-21 12:07:17 +1000

    Updated involved_identifiers to use varargs
    This means that any node with multiple entries (eg Assign.targets) can be
    passed in directly, eg:
    
        TutorialNodeVisitor.involved_identifiers(*node.targets)

--- code/tutorlib/analyser.py
+++ code/tutorlib/analyser.py
@@ -97,7 +97,7 @@ class TutorialNodeVisitor(ast.NodeVisitor):
         )
 
     @staticmethod
-    def involved_identifiers(node):
+    def involved_identifiers(*nodes):
         '''
         TutorialNodeVisitor.involved_identifiers(ast._AST) -> [str]
 
@@ -114,102 +114,114 @@ class TutorialNodeVisitor(ast.NodeVisitor):
         used in a statement, regardless of the complexity of the ast tree.
 
         '''
-        # if this node has an identifier, start out with that
-        this_identifier = TutorialNodeVisitor.identifier(
-            node, suppress_exceptions=True
-        )
-        identifiers = [this_identifier] if this_identifier is not None else []
+        identifiers = []
 
-        # set up what attrs we want to recurse on
-        # this is not always everything (eg, on a FunctionDef we don't want to
-        # consider the 'body' as having involved identifiers)
-        recurse_on = {
-            # stmt
-            ast.FunctionDef: ['args'],
-            ast.ClassDef: ['bases'],
-
-            ast.Delete: ['targets'],
-            ast.Assign: ['targets'],
-            ast.AugAssign: ['target', 'value'],
-
-            ast.For: ['target', 'iter'],
-            ast.While: ['test'],
-            ast.If: ['test'],
-            ast.With: ['items'],
-
-            ast.Assert: ['test', 'msg'],
-
-            ast.Import: ['names'],
-            ast.ImportFrom: ['names'],
-
-            ast.Expr: ['value'],
-
-            # expr
-            ast.BoolOp: ['values'],
-            ast.BinOp: ['left', 'right'],
-            ast.UnaryOp: ['operand'],
-            ast.Lambda: ['args'],
-            ast.IfExp: ['test'],
-            ast.Dict: ['keys', 'values'],
-            ast.Set: ['elts'],
-            ast.ListComp: ['generators'],
-            ast.SetComp: ['generators'],
-            ast.DictComp: ['generators'],
-            ast.GeneratorExp: ['generators'],
-
-            ast.Yield: ['value'],
-            ast.YieldFrom: ['value'],
-
-            ast.Compare: ['left', 'comparators'],
-            ast.Call: ['func', 'args', 'keywords', 'starargs', 'kwargs'],
-
-            ast.Attribute: ['value'],
-            ast.Subscript: ['value', 'slice'],
-            ast.Starred: ['value'],
-            ast.List: ['elts'],
-            ast.Tuple: ['elts'],
-
-            # slice
-            ast.Slice: ['lower', 'upper', 'step'],
-            ast.ExtSlice: ['dims'],
-            ast.Index: ['value'],
-
-            # comprehension
-            ast.comprehension: ['target', 'iter', 'ifs'],
-
-            # arguments, arg, keyword
-            ast.arguments: ['args', 'vararg', 'kwonlyargs', 'kw_defaults',
-                            'kwarg', 'defaults'],
-            ast.arg: ['annotation'],
-            ast.keyword: ['value'],
-
-            # withitem
-            ast.withitem: ['context_expr', 'optional_vars'],
-        }
-
-        # if we don't have anything to recurse on, we're done
-        if type(node) not in recurse_on:
-            return identifiers
-
-        # recursively check for identifiers
-        # note that some of the attrs (eg ClassDef.bases) are themselves
-        # sequences; because ast seems to always use list for these, we can
-        # just check for that
-        for attr_name in recurse_on[type(node)]:
-            attr_value = getattr(node, attr_name)
-
-            if isinstance(attr_value, list):
-                child_nodes = attr_value
-            else:
-                child_nodes = [attr_value]
-
-            # certain attrs are optional; ignore them if they're None
-            child_nodes = filter(None, child_nodes)
-
-            for child_node in child_nodes:
-                child_identifiers = TutorialNodeVisitor.involved_identifiers(
-                    child_node
+        for node in nodes:
+            if not isinstance(node, ast.AST):
+                raise StaticAnalysisError(
+                    'TutorialNodeVisitor.involved_identifiers must be '
+                    'called on ast nodes (got {!r} instead).  Did you mean '
+                    'to call this method with *varargs?  You called it with '
+                    '{!r} instead'.format(node, nodes)
                 )
-                identifiers.extend(child_identifiers)
+
+            # if this node has an identifier, start out with that
+            this_identifier = TutorialNodeVisitor.identifier(
+                node, suppress_exceptions=True
+            )
+            if this_identifier is not None:
+                identifiers.append(this_identifier)
+
+            # set up what attrs we want to recurse on
+            # this is not always everything (eg, on a FunctionDef we don't want
+            # to consider the 'body' as having involved identifiers)
+            recurse_on = {
+                # stmt
+                ast.FunctionDef: ['args'],
+                ast.ClassDef: ['bases'],
+
+                ast.Delete: ['targets'],
+                ast.Assign: ['targets', 'value'],
+                ast.AugAssign: ['target', 'value'],
+
+                ast.For: ['target', 'iter'],
+                ast.While: ['test'],
+                ast.If: ['test'],
+                ast.With: ['items'],
+
+                ast.Assert: ['test', 'msg'],
+
+                ast.Import: ['names'],
+                ast.ImportFrom: ['names'],
+
+                ast.Expr: ['value'],
+
+                # expr
+                ast.BoolOp: ['values'],
+                ast.BinOp: ['left', 'right'],
+                ast.UnaryOp: ['operand'],
+                ast.Lambda: ['args'],
+                ast.IfExp: ['test'],
+                ast.Dict: ['keys', 'values'],
+                ast.Set: ['elts'],
+                ast.ListComp: ['generators'],
+                ast.SetComp: ['generators'],
+                ast.DictComp: ['generators'],
+                ast.GeneratorExp: ['generators'],
+
+                ast.Yield: ['value'],
+                ast.YieldFrom: ['value'],
+
+                ast.Compare: ['left', 'comparators'],
+                ast.Call: ['func', 'args', 'keywords', 'starargs', 'kwargs'],
+
+                ast.Attribute: ['value'],
+                ast.Subscript: ['value', 'slice'],
+                ast.Starred: ['value'],
+                ast.List: ['elts'],
+                ast.Tuple: ['elts'],
+
+                # slice
+                ast.Slice: ['lower', 'upper', 'step'],
+                ast.ExtSlice: ['dims'],
+                ast.Index: ['value'],
+
+                # comprehension
+                ast.comprehension: ['target', 'iter', 'ifs'],
+
+                # arguments, arg, keyword
+                ast.arguments: ['args', 'vararg', 'kwonlyargs', 'kw_defaults',
+                                'kwarg', 'defaults'],
+                ast.arg: ['annotation'],
+                ast.keyword: ['value'],
+
+                # withitem
+                ast.withitem: ['context_expr', 'optional_vars'],
+            }
+
+            # if we don't have anything to recurse on, we're done
+            if type(node) not in recurse_on:
+                continue
+
+            # recursively check for identifiers
+            # note that some of the attrs (eg ClassDef.bases) are themselves
+            # sequences; because ast seems to always use list for these, we can
+            # just check for that
+            for attr_name in recurse_on[type(node)]:
+                attr_value = getattr(node, attr_name)
+
+                if isinstance(attr_value, list):
+                    child_nodes = attr_value
+                else:
+                    child_nodes = [attr_value]
+
+                # certain attrs are optional; ignore them if they're None
+                child_nodes = filter(None, child_nodes)
+
+                for child_node in child_nodes:
+                    child_ids = TutorialNodeVisitor.involved_identifiers(
+                        child_node
+                    )
+                    identifiers.extend(child_ids)
 
         return identifiers
