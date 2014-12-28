54394017c3aa785a18e25bb19c20257b00f1302b
sapi <sean.purdon@gmail.com>
2014-12-27 21:33:40 -0800

    Refactored tutorlib/TutorEditor.py and associated files
    This is not a complete refactor; a lot of things have been left the
    same as before.
    
    The whole menu system is clunky as hell.  I'll attack that (probably)
    when it comes time to deal with TutorialApp.py

--- code/tutorlib/editor/editor_window.py
+++ code/tutorlib/editor/editor_window.py
@@ -22,16 +22,13 @@
 import os
 from tkinter import *
 
-from idlelib import EditorWindow, FileList, macosxSupport
-from idlelib import WindowList
-#from idlelib.configHandler import idleConf
-from . import TutorBindings as tut_bindings
-from . import TutorIOBinding as tut_iobinding
+from idlelib import EditorWindow, macosxSupport
+import tutorlib.editor.bindings as Bindings  # consistent naming with idlelib
+from tutorlib.editor.io_binding import TutorIOBinding
 from tutorlib.gui.aboutdialog import TutAboutDialog
 from tutorlib.gui.helpdialog import HelpDialog
 import tkinter.filedialog
 import tkinter.messagebox
-#from TutorialData import *
 
 
 class TutorEditor(EditorWindow.EditorWindow):
@@ -45,24 +42,31 @@ class TutorEditor(EditorWindow.EditorWindow):
         ("help", "_Help"),
     ]
 
-    Bindings = tut_bindings
+    Bindings = Bindings
 
     def __init__(self, parent, flist=None, root=None,
                  filename=None, online=False):
 
-        # Support for Python >= 2.7.7 (TODO find a better way)
-        if hasattr(macosxSupport, "_initializeTkVariantTests") and macosxSupport._tk_type is None:
-            macosxSupport._initializeTkVariantTests(root)
+        ## Support for Python >= 2.7.7 (TODO find a better way)
+        #if hasattr(macosxSupport, "_initializeTkVariantTests") and macosxSupport._tk_type is None:
+        #    macosxSupport._initializeTkVariantTests(root)
 
         if online:
             self.menu_specs.insert(4, ("online", "_Online"))
-        EditorWindow.EditorWindow.__init__(self,  # flist = flist,
-                                           root=root, filename=filename)
-        self.io = io = tut_iobinding.TutorIOBinding(self)
-        io.set_filename_change_hook(self.filename_change_hook)
+
+        EditorWindow.EditorWindow.__init__(self, root=root, filename=filename)
+
+        self.io = TutorIOBinding(self)
+        self.io.set_filename_change_hook(self.filename_change_hook)
+
         self.parent = parent
         self.root = root
-        #self.load_extensions()
+
+        # TODO: I have absolutely no idea why some of these events feel the
+        # TODO: need to break out of the tk event loop (by return 'break',
+        # TODO: naturally...stupid tk)
+        # TODO: Because I don't want to break anything, I've left them as is
+        # TODO: for now, but it's worth looking into this later
         self.text.bind("<<load-from>>", self.load_from_event)
         self.text.bind("<<revert>>", self.revert_event)
         self.text.bind("<<check>>", self.check_event)
@@ -70,30 +74,35 @@ class TutorEditor(EditorWindow.EditorWindow):
         self.text.bind("<<help-tutor>>", self.help)
         self.text.bind("<F5>", self.check_event)
         self.text.bind("<F6>", self.submit_answer_event)
+
         if online:
-            self.text.bind("<<login>>", self.login_event)
-            self.text.bind("<<logout>>", self.logout_event)
-            self.text.bind("<<change_password>>", self.change_password_event)
-            self.text.bind("<<upload_answer>>", self.upload_answer_event)
-            self.text.bind("<<download_answer>>", self.download_answer_event)
+            self.text.bind("<<login>>", lambda e: self.parent.login())
+            self.text.bind("<<logout>>", lambda e: self.parent.logout())
+            self.text.bind(
+                "<<change_password>>", lambda e: self.parent.change_password()
+            )
+            self.text.bind(
+                "<<upload_answer>>", lambda e: self.parent.upload_answer()
+            )
+            self.text.bind(
+                "<<download_answer>>", lambda e: self.parent.download_answer()
+            )
             self.text.bind("<<submit_answer>>", self.submit_answer_event)
-            self.text.bind("<<show_submit>>", self.show_submit_event)
+            self.text.bind(
+                "<<show_submit>>", lambda e: self.parent.show_submit()
+            )
+
         self.text.tag_config("orange", background="orange")
+
         self.filename = ''
         self.dirname = ''
         self.opendialog = None
         self.menudict['file'].delete(0, 1)
-        #self.top = top = WindowList.ListedToplevel(root, menu=self.menubar)
-        #self.set_close_hook(self.quit)
-        self.top.protocol("WM_DELETE_WINDOW", self.close_event)
-
-    def close_event(self, _event=None):
-        pass
 
     def update_font(self, font_size):
-        self.text.config(font=('courier',
-                               str(int(font_size)+1),
-                               'normal', 'roman'))
+        self.text.config(
+            font=('courier', str(int(font_size)+1), 'normal', 'roman')
+        )
 
     def revert_event(self, e):
         reply = self.possiblysave("Save on Revert")
@@ -113,27 +122,9 @@ class TutorEditor(EditorWindow.EditorWindow):
         self.parent.run_tests()
         return "break"
 
-    def login_event(self, e):
-        self.parent.login()
-
-    def logout_event(self, e):
-        self.parent.logout()
-
-    def change_password_event(self, e):
-        self.parent.change_password()
-
-    def upload_answer_event(self, e):
-        self.parent.upload_answer()
-
-    def download_answer_event(self, e):
-        self.parent.download_answer()
-
     def submit_answer_event(self, e):
         self.parent.submit_answer()
         return "break"
-
-    def show_submit_event(self, e):
-        self.parent.show_submit()
 
     def possiblysave(self, title):
         reply = "no"
