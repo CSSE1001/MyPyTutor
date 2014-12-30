ce544c874613f31e63725301ac557c3dba953704
sapi <sean.purdon@gmail.com>
2014-12-30 00:35:11 -0800

    Added function/class comments

--- code/tutorlib/interface/alarm.py
+++ code/tutorlib/interface/alarm.py
@@ -3,18 +3,42 @@ import _thread
 import time
 
 
-# An alarm for setting a timeout on user code execution.
 class Alarm(Thread):
+    """
+    An alarm for setting a timeout on user code execution.
+
+    Attributes:
+      secs (float): The time, in seconds, to sleep (before firing the alarm).
+      do_interrupt (bool): Whether or not the alarm should fire after the
+          sleep time has elapsed.
+
+    """
     def __init__(self, secs):
+        """
+        Initialise a new Alarm object.
+
+        Args:
+          secs (float): The time, in seconds, to sleep (before firing the alarm).
+
+        """
         self.secs = secs
         self.do_interrupt = True
 
         super().__init__()
 
     def run(self):
+        """
+        Sleep for `.secs` seconds, then, if the alarm has not been cancelled,
+        interrupt the main thread.
+
+        """
         time.sleep(self.secs)
         if self.do_interrupt:
             _thread.interrupt_main()
 
     def stop_interrupt(self):
+        """
+        Cancel the alarm.
+
+        """
         self.do_interrupt = False
--- code/tutorlib/interface/problems.py
+++ code/tutorlib/interface/problems.py
@@ -4,11 +4,36 @@ from tutorlib.interface.tutorial import Tutorial
 
 
 class TutorialPackageError(Exception):
+    """
+    An error encountered in loading a tutorial package.
+
+    """
     pass
 
 
 class ProblemSet():
+    """
+    A problem set (collection of tutorial problems due on the same date, or
+    related in some other logical manner).
+
+    Problem sets are iterable objects (and will iterate over their component
+    problems).
+
+    Attributes:
+      name (str): The name of the problem set.
+      date (str): The due date of the problem set.
+      problems ([Tutorial]): The problems in the problem set.
+
+    """
     def __init__(self, name, date):
+        """
+        Create a new ProblemSet object.
+
+        Args:
+          name (str): The name of the problem set.
+          date (str): The due date of the problem set.
+
+        """
         self.name = name
         self.date = date
 
@@ -18,17 +43,60 @@ class ProblemSet():
         return 'ProblemSet({!r}, {!r})'.format(self.name, self.date)
 
     def __iter__(self):
+        """
+        Iterate over the problems in the problem set.
+
+        Returns:
+          An iterator over the problems in the problem set.
+
+        """
         return iter(self.problems)
 
     def add_problem(self, tutorial):
+        """
+        Add a new tutorial problem to the problem set.
+
+        Args:
+          tutorial (Tutorial): The tutorial to add.
+
+        """
         self.problems.append(tutorial)
 
 
 class TutorialPackage():
+    """
+    A tutorial package (collection of problem sets).
+
+    Class Attributes:
+      TUTORIALS_FILE (constant): The filename of the configuration file
+          containing details on problem sets.
+      CONFIG_FILE (constant): The filename of the configuration file containing
+          general configuration information on this tutorial package.
+
+    Attributes:
+      name (str): The name of the tutorial package.
+      options (Namespace): Tutorial package options (as taken from the
+          MyPytutor config data).  Contains `.tut_dir` and `.ans_dir`.
+
+    """
     TUTORIALS_FILE = 'tutorials.txt'
     CONFIG_FILE = 'config.txt'
 
     def __init__(self, name, options):
+        """
+        Initialise a new TutorialPackage object.
+
+        Args:
+          name (str): The name of the tutorial package.
+          options (Namespace): Tutorial package options (as taken from the
+              MyPyTutor config data).
+
+        Raises:
+          TutorialPackageError: If any error is encountered while parsing the
+             tutorial package, its component problem sets, or their component
+             tutorial problems.
+
+        """
         self.name = name
         self.options = options
 
@@ -51,6 +119,21 @@ class TutorialPackage():
             raise ex from e
 
     def _parse_config_file(self, f):
+        """
+        Parse the tutorial package configuration file.
+
+        The expected format of the file has two lines.
+        The first contains a timestamp.
+        The second contains a url.
+
+        Args:
+          f (file): The tutorial package configuration file.
+
+        Raises:
+          TutorialPackageError: If the configuration file is not in the
+              expected format.
+
+        """
         # expected format
         # timestamp
         # url
@@ -65,11 +148,23 @@ class TutorialPackage():
         self.timestamp = float(timestamp)
 
     def _parse_tutorials_file(self, f):
-        # expected file format:
-        # [date section_name]
-        # problem_name:problem_directory
-        # ...
+        """
+        Parse the problem set configuration file (list of tutorials).
 
+        The file must have one or more section headers, of the format:
+            [date section_name]
+
+        followed by one or more problem descriptions, of the format:
+            problem name:directory.tut
+
+        Args:
+          f (file): The problem set configuration file.
+
+        Raises:
+          TutorialPackageError: If the configuration file is not in the
+              expected format, or if any of the tutorials do not exist.
+
+        """
         self.problem_sets = []
         problem_set = None
 
@@ -117,6 +212,28 @@ class TutorialPackage():
                 problem_set.add_problem(tutorial)
 
     def _get_tutorial(self, current_tutorial, get_previous=True):
+        """
+        Get the next or previous tutorial after or before the given tutorial.
+
+        Args:
+          current_tutorial (Tutorial): The current tutorial (to base the next
+              or previous tutorial decision off).
+          get_previous (bool, optional): Whether to get the previous tutorial.
+              Defaults to True.  If False, the next tutorial will be returned.
+
+        Returns:
+          The next or previous tutorial (as a Tutorial object).
+
+          If the current tutorial is None, then the first tutorial will be
+          returned.
+
+          If the current tutorial is the first tutorial, and the previous
+          tutorial is requested, then the first tutorial will be returned.
+
+          If the current tutorial is the last tutorial, and the next tutorial
+          is requested, then the last tutorial will be returned.
+
+        """
         previous_tutorial = None
         return_next = current_tutorial is None  # return first if None
 
@@ -133,12 +250,59 @@ class TutorialPackage():
         return tutorial  # if at end, return same
 
     def next(self, current_tutorial):
+        """
+        Get the next tutorial after the given tutorial.
+
+        Args:
+          current_tutorial (Tutorial): The current tutorial.
+
+        Returns:
+          The next tutorial (as a Tutorial object).
+
+          If the current tutorial is None, then the first tutorial will be
+          returned.
+
+          If the current tutorial is the last tutorial, then this method will
+          return the last (current) tutorial.
+
+        """
         return self._get_tutorial(current_tutorial, get_previous=False)
 
     def previous(self, current_tutorial):
+        """
+        Get the previous tutorial before the given tutorial.
+
+        Args:
+          current_tutorial (Tutorial): The current tutorial.
+
+        Returns:
+          The next tutorial (as a Tutorial object).
+
+          If the current tutorial is None, then the first tutorial will be
+          returned.
+
+          If the current tutorial is the first tutorial, then this method
+          will return the first (current) tutorial.
+
+        """
         return self._get_tutorial(current_tutorial, get_previous=True)
 
     def tutorial_with_name(self, tutorial_name):
+        """
+        Return the tutorial with the given name.
+
+        Args:
+          tutorial_name (str): The name of the tutorial to return.
+
+        Returns:
+          The tutorial with the name `tutorial_name`.
+
+          If no such tutorial exists, return None.
+
+          If multiple tutorials with the given name exist, the first (with the
+          earliest appearance in the earliest problem set) will be returned.
+
+        """
         for problem_set in self.problem_sets:
             for tutorial in problem_set:
                 if tutorial.name == tutorial_name:
