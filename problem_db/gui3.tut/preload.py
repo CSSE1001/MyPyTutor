3b0ce9a30e2c18e86e3299b6805b50dee135fb30
sapi <sean.purdon@gmail.com>
2015-01-18 02:26:22 -0800

    Added ascii art

--- problem_db/gui3.tut/preload.py
+++ problem_db/gui3.tut/preload.py
@@ -14,20 +14,19 @@ def pressed():
 
 def create_layout(frame):
     """
-    Add two buttons to the frame.
+    Add four buttons to the frame in the given formt.
 
     Both buttons should have the callback (command) pressed, and they should
-    have the labels "Button1" and "Button2".
+    have the labels "Button1" through "Button4".
 
     The layout in the frame after running this function will be:
-      +------------------------------+
-      |    |(20px)                   |
-      |[Button1] <------------------ |
-      |    |(20px)                   |
-      |[-(20px)-Button2-(20px)-] <-- |
-      |                              |
-      |                              |
-      +------------------------------+
+      +---------------------------------------+
+      |                                       |
+      |  [Button1]                            |
+      |               [Button3]    [Button4]  |
+      |  [Button2]                            |
+      |                                       |
+      +---------------------------------------+
 
     Args:
       frame (tk.Frame): The frame to create the two buttons in.
