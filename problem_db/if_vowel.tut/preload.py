124e1ffded88ff68c8d1bc7d1b321fa5255e3855
pjr <pjr@itee.uq.edu.au>
2015-02-05 08:29:50 +1000

    Add alt defn of is_vowel and fix </tt> error

--- problem_db/if_vowel.tut/preload.py
+++ problem_db/if_vowel.tut/preload.py
@@ -1,5 +1,9 @@
 def is_vowel(c):
     return c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u'
 
+# The above definition uses or, below is an alternative definition that uses in 
+#
+# def is_vowel(c):
+#     return c in 'aeiou'
 
 char = input('Please enter a character: ')
