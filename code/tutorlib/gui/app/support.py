fd6b35d40939449f55a0da576eacaeae61048d3a
Sean Purdon <sean.purdon@gmail.com>
2015-02-16 14:25:01 +1100

    Removed old code

--- code/tutorlib/gui/app/support.py
+++ code/tutorlib/gui/app/support.py
@@ -4,27 +4,6 @@ import shutil
 import zipfile
 
 
-def unzipit(zf, path):
-    z = zipfile.ZipFile(zf)
-    info = z.namelist()
-    if not os.path.exists(path):
-        os.mkdir(path)
-    for item in info:
-        if item.endswith('/') or item.endswith('\\'):
-            fulldir = os.path.join(path, item)
-            if not os.path.exists(fulldir):
-                os.mkdir(fulldir)
-        else:
-            flags = (z.getinfo(item).external_attr >> 16) & 0o777
-            text = z.read(item)
-            fullpath = os.path.join(path, item)
-            fd = open(fullpath, 'wb')
-            fd.write(text)
-            fd.close()
-            os.chmod(fullpath, flags)
-    z.close()
-
-
 def safely_extract_zipfile(zip_path, extraction_path):
     """
     Safely extract a zipfile, with defence against path traversal attacks.
