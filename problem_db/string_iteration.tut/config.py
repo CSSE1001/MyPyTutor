20165237b0929ea71d426f2273b4bf0f7cec51c3
pjr <pjr@itee.uq.edu.au>
2015-02-18 07:57:35 +1000

    Add minor fixes to 2nd lot of problems

--- problem_db/string_iteration.tut/config.py
+++ problem_db/string_iteration.tut/config.py
@@ -1,6 +1,6 @@
 SHORT_DESCRIPTION = 'Write a function occurrences(text1, text2) which returns the number of times a character from the first argument appears in the second argument'
 STUDENT_FUNCTION = 'occurrences'
 HINTS = [
-    'The in operator is likely to be useful',
-    'Make sure you have the loops the right way around (look at the examples)',
+    'The <tt>in</tt> operator is likely to be useful',
+    'Make sure you have the loops the right way around (look at the test cases)',
 ]
