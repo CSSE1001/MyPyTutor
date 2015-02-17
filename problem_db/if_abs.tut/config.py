08c0f7e18ae989bfb4177372ff2ac58ee6df9136
pjr <pjr@itee.uq.edu.au>
2015-02-18 07:34:48 +1000

    Update intro problems with minor changes to descriptions

--- problem_db/if_abs.tut/config.py
+++ problem_db/if_abs.tut/config.py
@@ -2,5 +2,4 @@ SHORT_DESCRIPTION = 'Use an if statement to print the absolute value of a number
 WRAP_STUDENT_CODE = True
 HINTS = [
     'You can make a negative number positive by multiplying it by -1',
-    'Try, eg, number = number*-1',
 ]
