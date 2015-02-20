aeef2ab45eb5896904e54c970f0f9aa79d7c0154
Peter Robinson <pjr@itee.uq.edu.au>
2015-02-20 10:09:24 +1000

    fix repeated dict problems and general updates

--- problem_db/dict_iteration.tut/analysis.py
+++ problem_db/dict_iteration.tut/analysis.py
@@ -2,26 +2,41 @@ class CodeVisitor(TutorialNodeVisitor):
     def __init__(self):
         super().__init__()
 
-        self.has_if_statement = False
+        self.has_for = False
+        self.iteration_variable = None
 
-    def visit_If(self, node):
-        super().visit_If(node)
+    def visit_For(self, node):
+        super().visit_For(node)
 
-        self.has_if_statement = True
+        if self._current_function == 'big_keys':
+            self.has_for = True
+
+            self.iteration_variable = TutorialNodeVisitor.identifier(node.iter)
 
 
 class DictAnalyser(CodeAnalyser):
     def _analyse(self):
-        if not self.visitor.functions['get_value'].is_defined:
-            self.add_error('You need to define a get_value function')
-        elif len(self.visitor.functions['get_value'].args) != 2:
-            self.add_error('get_value must accept exactly two args')
+        if not self.visitor.functions['big_keys'].is_defined:
+            self.add_error('You need to define a big_keys function')
+        elif len(self.visitor.functions['big_keys'].args) != 2:
+            self.add_error('big_keys must accept exactly two args')
 
-        if not self.visitor.functions['get_value'].calls['get']:
-            self.add_error('You need to use the "get" method of your dictionary')
+        if not self.visitor.has_for:
+            self.add_error(
+                'Your function definition does not contain a for loop.'
+            )
+        else:
+            if self.visitor.functions['big_keys'].is_defined \
+               and (self.visitor.functions['big_keys'].args[0] \
+                    != self.visitor.iteration_variable):
+                self.add_warning(
+                    'Your for loop should iterate over {}'.format(
+                        self.visitor.functions['big_keys'].args[0]
+                    )
+                )
 
-        if self.visitor.has_if_statement:
-            self.add_error('You do not need to use an if statement')
+        if not self.visitor.functions['big_keys'].returns:
+            self.add_error('You need a return statement.')
 
 
 ANALYSER = DictAnalyser(CodeVisitor)
