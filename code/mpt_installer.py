62cae198c257b193d7771d71e5c1aef48832a869
Sean Purdon <sean.purdon@gmail.com>
2015-02-20 18:25:32 +1100

    Consistent extraction method with rest of app

--- code/mpt_installer.py
+++ code/mpt_installer.py
@@ -53,26 +53,10 @@ ans_dir =
 
 def unzipfile(zf, path):
     """Unzip the zip file zf in the folder path."""
+    with zipfile.ZipFile(zf) as zf:
+        return zf.extractall(path)
 
-    z = zipfile.ZipFile(zf)
-    info = z.namelist()
-    if not os.path.exists(path):
-        os.mkdir(path)
-    for item in info:
-        if item.endswith('/') or item.endswith('\\'):
-            fulldir = os.path.join(path,item)
-            if not os.path.exists(fulldir):
-                os.mkdir(fulldir)
-        else:
-            flags = (z.getinfo(item).external_attr >> 16) & 0o777
-            text = z.read(item)
-            fullpath = os.path.join(path,item)
-            fd = open(fullpath, 'wb')
-            fd.write(text)
-            fd.close()
-            os.chmod(fullpath, flags)
-    z.close()
-   
+
 class InstallDirDialog(Toplevel):
     """Dialog for choosing installation folder."""
 
