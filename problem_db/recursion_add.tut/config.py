a7479feb22d58dfd005fd8b9164e407f3cc280a4
pjr <pjr@itee.uq.edu.au>
2015-02-26 11:07:47 +1000

    remove recursion_add problem

--- problem_db/recursion_add.tut/config.py
+++
@@ -1,6 +0,0 @@
-SHORT_DESCRIPTION = 'Return a list of the digits in the given number'
-HINTS = [
-    'You should use integer division (//) and remainder (%)',
-    '124 == 12*10 + 4     124/10 == 12     124%10 == 4',
-    '[1,2,4] == [1,2] + [4]',
-]
