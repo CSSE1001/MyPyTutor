17414ce1ed998b0f234677b129460b132bbf8dbf
Ben Martin <ben@benmartinstudios.com.au>
2015-02-27 15:16:39 +1000

    Fixed assertion failure message.

--- code/tutorlib/config/shared.py
+++ code/tutorlib/config/shared.py
@@ -6,7 +6,7 @@ if not os.path.exists(MPT_DIR):
     os.mkdir(MPT_DIR)
 else:
     assert os.path.isdir(MPT_DIR), \
-        '{} should be a directory.  Please delete the existing config '
+        '{} should be a directory.  Please delete the existing config ' \
     'file at that location.'.format(MPT_DIR)
 
 CONFIG_FILE = os.path.join(MPT_DIR, 'config')
