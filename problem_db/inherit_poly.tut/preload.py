2565f36e5888634592308181330a023ec8b033c0
Sean Purdon <sean.purdon@gmail.com>
2015-03-02 09:08:44 +1000

    Added basics of Shape question

--- problem_db/inherit_poly.tut/preload.py
+++ problem_db/inherit_poly.tut/preload.py
@@ -1,7 +1,31 @@
-class TicTacToe():
-    def __init__(self, player1, player2):
-        self.players = [player1, player2]
+class Shape():
+    def area(self):
+        raise NotImplementedError()
 
 
-class Player():
-    def
+class Square(Shape):
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def area(self):
+        return self.side_length * self.side_length
+
+
+def total_area(shapes):
+    """
+    Return the total area of the given list of shapes.
+
+    Args:
+      shapes ([Shape]): The list of shapes to sum the area for.
+
+    Returns:
+      The total area of the list of shapes, being the sum of the area of each
+      individual shape.
+
+    """
+    area = 0.
+
+    for shape in shapes:
+        area += shape.area()
+
+    return area
