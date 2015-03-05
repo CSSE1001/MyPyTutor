382e812c0a7401ee8b2058b15a96976709aa9cf3
Sean Purdon <sean.purdon@gmail.com>
2015-03-05 19:24:37 +1000

    Updated tutorials in response to student feedback

--- problem_db/string_slicing.tut/config.py
+++ problem_db/string_slicing.tut/config.py
@@ -1,4 +1,4 @@
-SHORT_DESCRIPTION = 'Write the functions slice_from(string, start, end) and reverse(string)'
+SHORT_DESCRIPTION = 'Write the functions slice_from(string, start, end) and reverse_string(string)'
 STUDENT_FUNCTION = 'slice_from'
 HINTS = [
     'Remember that the slicing syntax is object[start:end:step]',
