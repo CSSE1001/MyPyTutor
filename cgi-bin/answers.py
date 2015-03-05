8c33b29ff09207bbd45f52a1cac043596b816a96
Sean Purdon <sean.purdon@gmail.com>
2015-03-05 21:17:29 +1000

    You can now view students' (synchronised) answers

---
+++ cgi-bin/answers.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import cgi
+from mako.template import Template
+from mako import exceptions
+
+from admin import admin_init
+import support
+
+
+def main():
+    if not admin_init():
+        return
+
+    form = cgi.FieldStorage(keep_blank_values=True)
+
+    if not all(val in form for val in ('user', 'pset', 'tutorial')):
+        print 'Invalid query params'
+        return
+
+    user = form.getvalue('user')
+    problem_set_name = form.getvalue('pset')
+    tutorial_name = form.getvalue('tutorial')
+
+    code = support.read_answer(
+        user, 'CSSE1001Tutorials', problem_set_name, tutorial_name
+    )
+
+    data = {
+        'user': user,
+        'pset': problem_set_name,
+        'tutorial': tutorial_name,
+        'code': code,
+    }
+
+    try:
+        print(Template(filename="./templates/answers.html").render(**data))
+    except:
+        print(exceptions.html_error_template().render())
+
+
+if __name__ == '__main__':
+    main()
