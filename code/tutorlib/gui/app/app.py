e7ef6129db7e822d96efcf62ce8544f563127488
Ben Martin <ben@benmartinstudios.com.au>
2016-09-05 17:34:38 +1000

    p

--- code/tutorlib/gui/app/app.py
+++ code/tutorlib/gui/app/app.py
@@ -30,7 +30,7 @@ from tutorlib.interface.web_api import WebAPI, WebAPIError
 from tutorlib.online.sync import SyncClient
 
 
-VERSION = '3.0.14'
+VERSION = '3.0.15'
 
 
 class TutorialApp(TutorialMenuDelegate, TutorEditorDelegate,
